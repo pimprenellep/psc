157ee4fa903e7b57f2ae6e77641c046e04a70583
Pimprenelle <31759861+pimprenellep@users.noreply.github.com>
2017-12-20 15:32:27 +0100

    Add files via upload

--- modelisation-3D/morphologie.py
+++ modelisation-3D/morphologie.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 13 16:48:00 2017
+Created on Wed Dec 20 15:26:13 2017
 
+@author: pimpim
 """
 
-class static morph:
+class morph :
     
-    def __init__(taille, poids):
+    def __init__(self,taille, poids):
         self.poids = {'tete': 0.09 * poids, 'tronc_bassin': 0.48 * poids,'avant-bras': 0.024 * poids,
-                    'bras': 0.028 * poids, 'cuisse': 0.1 * poids, 'mollet': 0.063 * poids};
+                    'bras': 0.028 * poids, 'cuisse': 0.1 * poids, 'mollet': 0.063 * poids}
         self.longeurs = {'tete': 0.1 * taille, 'tronc': 0.21 * taille, 'bassin': 0.08 * taille, 'epaule': 0.2 * taille,
-                         'avant-bras': 0.22 * taille, 'bras': 0.17 * taille, 'cuisse': 0.246 * taille, 'mollet': 0.325 * taille};
+                         'avant-bras': 0.22 * taille, 'bras': 0.17 * taille, 'cuisse': 0.246 * taille, 'mollet': 0.325 * taille}
         
-    def getLongeur(partie):
-        return self.longeurs[partie];
+    def getLongeur(self,partie):
+        return self.longeurs[partie]
     
-    def getPoid(partie):
-        return self.poids[partie];
+    def getPoid(self,partie):
+        return self.poids[partie]
+        
+        
