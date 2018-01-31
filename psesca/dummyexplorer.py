8dec9b409fb773de96b7d52a0f36b4ba7135edfc
Étienne Morice <pro.emorice@mail.com>
2018-01-31 17:28:48 +0100

    Clean design implementation of a climber model as a graph of body parts and code to pass it to ODE.

--- psesca/dummyexplorer.py
+++ psesca/dummyexplorer.py
@@ -1,13 +1,13 @@
 from .explorer import Explorer
 from .morphology import Morphology
-from .explicitclimbermodel import ExplicitClimberModel
+from .climbermodel import ClimberModel
 from .dummycontroller import DummyController
 
 # Does nothing but instantiating a controller
 class DummyExplorer(Explorer) :
     def __init__(self, stanceGraph):
         morphology = Morphology(170, 60)
-        self.climber = ExplicitClimberModel(morphology)
+        self.climber = ClimberModel(morphology)
         self.graph = stanceGraph
         self.controller = DummyController(self.climber, stanceGraph)
 
