980e4459870ec058fb6acaecd01578d0061107c8
Pimprenelle <31759861+pimprenellep@users.noreply.github.com>
2018-02-14 15:31:33 +0100

    on continue...

---
+++ modelisation-3D/prep.py
@@ -0,0 +1,73 @@
+import sys, os, random, time
+from math import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+
+# geometric utility functions
+def scalp (vec, scal):
+    vec[0] *= scal
+    vec[1] *= scal
+    vec[2] *= scal
+
+def length (vec):
+    return sqrt (vec[0]**2 + vec[1]**2 + vec[2]**2)
+    
+def echelle(pixel):
+    return (pixel*1)
+    
+## prepare_GL
+def prepare_GL():
+    """Prepare drawing.
+    """
+
+    # Viewport
+    glViewport(0,0,300,480) #x,y spécifie la position du coin gauche bas de la fenetre (en pixel), les deux chiffres suivants précisent la taille de l'image
+    
+
+    # Initialize
+    glClearColor(0.8,0.8,0.9,0)# couleur de l'arrière plan
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST)
+    glDisable(GL_LIGHTING)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_NORMALIZE)
+    glShadeModel(GL_FLAT)
+
+    # Projection
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective (45,1.3333,0.2,20)
+
+    # Initialize ModelView matrix
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    # Light source
+    glLightfv(GL_LIGHT0,GL_POSITION,[0,0,1,0])
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,[1,1,1,1])
+    glLightfv(GL_LIGHT0,GL_SPECULAR,[1,1,1,1])
+    glEnable(GL_LIGHT0)
+
+    # View transformation
+    gluLookAt (-10.0, 3.0, 1., 0., 0., 0, 0, 1, 0) #position of the eye point (3), position of the reference point (3), up vector (3)
+    #gluLookAt (3.4,3.6., 0., 0., 0., 0., 0., 1., 0.)
+
+
+# Initialize Glut
+glutInit ([])
+
+# Open a window
+glutInitDisplayMode (GLUT_RGB | GLUT_DOUBLE)
+
+x = 0
+y = 0
+width = 640
+height = 480
+glutInitWindowPosition (x, y);
+glutInitWindowSize (width, height);
+glutCreateWindow (b"testode")
+
+
+glutIdleFunc (_idlefunc)
