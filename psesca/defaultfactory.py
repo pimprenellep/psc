ca8727d76aed59a81e209ed727420f0f62b28feb
Etienne Morice <etienne.morice@atlas.polytechnique.fr>
2018-02-22 18:20:55 +0100

    Fixed build system for increased portability

--- psesca/defaultfactory.py
+++ psesca/defaultfactory.py
@@ -1,6 +1,5 @@
 from .dijkstraexplorer import DijkstraExplorer
 from .native import Controller
-from .renderer import Renderer
 
 class DefaultFactory:
 
@@ -8,5 +7,3 @@ class DefaultFactory:
         return DijkstraExplorer(*args)
     def buildController(self, *args):
         return Controller(*args)
-    def buildRenderer(self, *args):
-        return Renderer(*args)
