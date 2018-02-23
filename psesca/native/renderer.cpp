f802e26f5d177af54a8b13cd43133c1da5daa011
Étienne Morice <pro.emorice@mail.com>
2018-02-23 15:22:31 +0100

    context

--- psesca/native/renderer.cpp
+++ psesca/native/renderer.cpp
@@ -2,17 +2,41 @@
 
 #include <iostream>
 #include <cassert>
-#define EGL_EGLEXT_PROTOTYPES
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
 
-#define DIEIF(ok, msg)		\
-	if(!ok) {		\
+#define ERROR_CASE(E) \
+	case E:		\
+	std::cerr << "EGL error is " #E << std::endl; \
+	break;
+
+#define DIEIFN(ok, msg)		\
+	if(!(ok)) {		\
 		std::cerr << "Renderer : " msg << std::endl;	\
-		return;								\
+		switch(eglGetError()) {				\
+		ERRORS(ERROR_CASE)				\
+		}						\
+		return;						\
 	}
 
-Renderer::Renderer()
+#define ERRORS(X)	\
+	X(EGL_SUCCESS)		\
+	X(EGL_NOT_INITIALIZED)	\
+	X(EGL_BAD_ACCESS)	\
+	X(EGL_BAD_ALLOC)	\
+	X(EGL_BAD_ATTRIBUTE)	\
+	X(EGL_BAD_CONFIG)	\
+	X(EGL_BAD_CONTEXT)	\
+	X(EGL_BAD_CURRENT_SURFACE)	\
+	X(EGL_BAD_DISPLAY)	\
+	X(EGL_BAD_MATCH)	\
+	X(EGL_BAD_NATIVE_PIXMAP)	\
+	X(EGL_BAD_NATIVE_WINDOW)	\
+	X(EGL_BAD_PARAMETER)	\
+	X(EGL_BAD_SURFACE)	\
+	X(EGL_CONTEXT_LOST)
+
+Renderer::Renderer() :
+	display(EGL_NO_DISPLAY),
+	context(EGL_NO_CONTEXT)
 {
 	EGLBoolean ok;
 
@@ -25,7 +49,7 @@ Renderer::Renderer()
 		(PFNEGLQUERYDEVICESEXTPROC) eglGetProcAddress("eglQueryDevicesEXT");
 	if(eglQueryDevicesEXT) {
 		ok = eglQueryDevicesEXT(16, devices, &nDevices);
-		DIEIF(ok, "Cannot enumerate devices.");
+		DIEIFN(ok, "Cannot enumerate devices.");
 	}
 
 	std::cout << nDevices << " graphical devices found." << std::endl;
@@ -35,34 +59,76 @@ Renderer::Renderer()
 		(PFNEGLGETPLATFORMDISPLAYEXTPROC) eglGetProcAddress("eglGetPlatformDisplayEXT");
 	assert(eglGetPlatformDisplayEXT);
 
-	EGLDisplay dp;
 	if(nDevices) {
 		std::cout << "Devices found, trying to get device display." << std::endl;
-		dp = eglGetPlatformDisplayEXT(EGL_PLATFORM_DEVICE_EXT, EGL_DEFAULT_DISPLAY, 0);
+		display = eglGetPlatformDisplayEXT(EGL_PLATFORM_DEVICE_EXT, EGL_DEFAULT_DISPLAY, 0);
 	} else {
-		std::cout << "No devices found, trying to get mesa display." << std::endl;
-		dp = eglGetPlatformDisplayEXT(EGL_PLATFORM_GBM_MESA, EGL_DEFAULT_DISPLAY, 0);
+		std::cout << "No devices found, trying to get default display." << std::endl;
+		//display = eglGetPlatformDisplayEXT(EGL_PLATFORM_GBM_MESA, EGL_DEFAULT_DISPLAY, 0);
+		display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
 	}
-	if(dp == EGL_NO_DISPLAY) {
+	if(display == EGL_NO_DISPLAY) {
 		std::cerr << "Renderer : cannot get display." << std::endl;
 		return;
 	}
 
 	EGLint major, minor;
-	ok = eglInitialize(dp, &major, &minor);
+	ok = eglInitialize(display, &major, &minor);
 	if(!ok) {
 		std::cerr << "Renderer : cannot initialize display." << std::endl;
 		return;
 	}
 
-	std::cerr << eglQueryString(dp, EGL_VENDOR) << std::endl;
-	std::cerr << eglQueryString(dp, EGL_CLIENT_APIS) << std::endl;
-	std::cerr << eglQueryString(dp, EGL_EXTENSIONS) << std::endl;
+	std::cerr << eglQueryString(display, EGL_VENDOR) << std::endl;
+	std::cerr << eglQueryString(display, EGL_CLIENT_APIS) << std::endl;
+	std::cerr << eglQueryString(display, EGL_EXTENSIONS) << std::endl;
 
-	ok = eglBindAPI(EGL_OPENGL_ES_API);
-	DIEIF(ok, "Renderer : cannot bind api.")
+	ok = eglBindAPI(EGL_OPENGL_API);
+	DIEIFN(ok, "Renderer : cannot bind api.");
+
+	EGLConfig config;
+
+	// Defaults from nvidia devblog
+	static const EGLint configAttribs[] = {
+		EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+		EGL_BLUE_SIZE, 8,
+		EGL_GREEN_SIZE, 8,
+		EGL_RED_SIZE, 8,
+		EGL_DEPTH_SIZE, 8,
+		EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+		EGL_NONE
+	};
+	EGLint requires[] = {
+		EGL_RENDERABLE_TYPE,	EGL_OPENGL_BIT,
+		EGL_SURFACE_TYPE,	EGL_PBUFFER_BIT,
+		EGL_NONE };
+	int nConfigs = 0;
+
+	ok = eglGetConfigs(display, 0, 0, &nConfigs);
+	std::cerr << nConfigs << " configs available." << std::endl;
+	ok = eglChooseConfig(display, requires, 0, 0, &nConfigs);
+	std::cerr << nConfigs << " matching configs available." << std::endl;
+	ok = eglChooseConfig(display, requires, &config, 1, &nConfigs);
+	DIEIFN(ok && nConfigs, "Cannot get config.")
+	
+	context = eglCreateContext(display, config, EGL_NO_CONTEXT, 0);
+	DIEIFN(context != EGL_NO_CONTEXT, "Cannot create context");
+
+
+
+	std::cerr << "Api initialized :" << std::endl;
+	std::cerr << glGetString(GL_VENDOR) << std::endl;
+	std::cerr << glGetString(GL_RENDERER) << std::endl;
+	std::cerr << glGetString(GL_VERSION) << std::endl;
+	std::cerr << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+
 	
 	std::cout << "Renderer initialized (EGL "<<major<<"."<<minor<<")." << std::endl;
 
+}
+
+Renderer::~Renderer()
+{
+	eglTerminate(display);
 }
 
--- psesca/native/renderer.hpp
+++ psesca/native/renderer.hpp
@@ -1,9 +1,20 @@
 #ifndef _RENDERER_HPP
 #define _RENDERER_HPP
 
+#define EGL_EGLEXT_PROTOTYPES
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+#include <GL/gl.h>
+#include <GL/glext.h>
+
 class Renderer {
 	public:
 	       	Renderer();
+		~Renderer();
+	private:
+		EGLDisplay display;
+		EGLContext context;
 };
 
 #endif // _RENDERER_HPP
