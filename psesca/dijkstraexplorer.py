eb98c67499509cda08dd744e19dab90613f9bd42
Étienne Morice <pro.emorice@mail.com>
2018-03-07 17:51:36 +0100

    removing pkg-config

--- psesca/dijkstraexplorer.py
+++ psesca/dijkstraexplorer.py
@@ -7,35 +7,35 @@ class DijkstraExplorer(Explorer) :
         super().__init__(stanceGraph)
 
     def short_path(graph, source, destination):
-    previous, dist = dijkstra(graph, source)
-    full_path = deque()
-    current = previous[destination]
-    while current !=source :
-        full_path.appendleft(current)
-        current = previous[current]
-    full_path.appendleft(source)
-    full_path.append(destination)
-    return dist[destination], list(full_path)
+        previous, dist = dijkstra(graph, source)
+        full_path = deque()
+        current = previous[destination]
+        while current !=source :
+            full_path.appendleft(current)
+            current = previous[current]
+        full_path.appendleft(source)
+        full_path.append(destination)
+        return dist[destination], list(full_path)
 
     def dijkstra(graph, source):
-    n=len(graph.nodes)
-    dist=[10000000]*n
-    previous = [0]*n
-    for i in range(n):
-        previous[i]=-1
-    dist[source]=0
-    Queue = Heap()
-    Queue.push(0,source)
-    while len(Queue)>0 :
-        u= Queue.pop()
-        for node in graph.edges[u]:
-            print(u, node)
-            distance = dist[u]+graph.distances[(u,node)]
-            if(distance < dist[node]):
-                dist[node]= distance
-                previous[node]=u
-                Queue.push(distance,node)
-    return previous, dist
+        n=len(graph.nodes)
+        dist=[10000000]*n
+        previous = [0]*n
+        for i in range(n):
+            previous[i]=-1
+        dist[source]=0
+        Queue = Heap()
+        Queue.push(0,source)
+        while len(Queue)>0 :
+            u= Queue.pop()
+            for node in graph.edges[u]:
+                print(u, node)
+                distance = dist[u]+graph.distances[(u,node)]
+                if(distance < dist[node]):
+                    dist[node]= distance
+                    previous[node]=u
+                    Queue.push(distance,node)
+        return previous, dist
 
 
     def findPath(self):
