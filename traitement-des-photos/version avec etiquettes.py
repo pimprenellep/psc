581d1fdb8bdab8237872b25daa295695cc6d626f
Pimprenelle <31759861+pimprenellep@users.noreply.github.com>
2018-03-14 16:49:53 +0100

    On calcule l'échelle de la voie grâce à une feuille blanche placée sur le mur

--- traitement-des-photos/version avec etiquettes.py
+++ traitement-des-photos/version avec etiquettes.py
@@ -4,8 +4,8 @@ import numpy as np
 from math import *
 
 im = Image.new("RGB", (512, 512), "white")
-img=Image.open('copiedelespoir.bmp')
-color_thief = ColorThief('copiedelespoir.bmp')
+img=Image.open('copiedelespoir3.bmp')
+color_thief = ColorThief('copiedelespoir3.bmp')
 # get the dominant color
 dominant_color = color_thief.get_color(quality=1)
 # build a color palette
@@ -15,6 +15,16 @@ r=dominant_color[0]
 b=dominant_color[1]
 g=dominant_color[2]
 
+def max_liste(liste):
+    maxi = liste[0]
+    ind_max = 0
+    k=0
+    for h in liste :
+        if h>maxi:
+            maxi = h
+            ind_maxi = k
+        k=k+1
+    return(maxi,ind_maxi)
 
 imgN=Image.new(img.mode,img.size)
 imgNN=Image.new(img.mode,img.size)
@@ -37,9 +47,11 @@ imgN.save('imgN.png')
 n=max(l,c)
 taille_pixel_h=0
 taille_pixel_l=0
+
 #n=5
 #M=[[0,0,1,0,0],[0,1,1,1,0],[0,0,0,1,0],[0,0,0,0,0],[1,1,1,1,0]]
 Liste_barycentres=[]
+colors = []
 for i in range(l-1):
     for j in range(c-1):
         if L[i][j]==1:
@@ -47,7 +59,8 @@ for i in range(l-1):
             bas_max=i
             gauche_max=j
             droite_max=j
-            color_min=(256,256,256)
+            somme_color = [0,0,0]
+            color = []
             L[i][j]=2
             nb_pixel=1
             #on parcourt les cases adjacentes
@@ -71,6 +84,7 @@ for i in range(l-1):
                 for (i, j) in indicesAdjacentes:
                      if i >= 0 and j >= 0 and i < l-1 and j < c-1 and L[i][j]==1 and not dejavues[i][j]:
                          adjacentes += [(i, j)]
+                '''calcul des dimensions de la prise'''
                 for (i, j) in adjacentes:
                     if j>droite_max:
                         droite_max=j
@@ -81,28 +95,38 @@ for i in range(l-1):
                     if bas_max>i:
                         bas_max=i
                     pixel=img.getpixel((i,j))
-                    if color_min[0]>pixel[0]:
-                        color_min[0]=pixel[0]
-                    if color_min[1]>pixel[1]:
-                        color_min[1]=pixel[1]
-                    if color_min[2]>pixel[2]:
-                        color_min[2]=pixel[2]
+                    '''collecte des couleurs de la prise'''
+                    color.append([pixel[0],pixel[1],pixel[2]])
                     nb_pixel+=1
                     dejavues[i][j] = True
                     L[i][j]=2
                     p.append((i,j))
-            if(nb_pixel>(l*c)/4800):        
-                Liste_barycentres.append([int(floor((haut_max+bas_max)/2)),int(floor((gauche_max+droite_max)/2)), int(floor((haut_max - bas_max)/2)), int(floor((droite_max - gauche_max)/2)), nb_pixel])
-                if((color_min[0]<10)and(color_min[1]<10)and(color_min[2]<10)):
-                    echelle = 0,24/(haut_max-bas_max)
-                
+            if(nb_pixel>(l*c)/4800):
+                for col in color : 
+                    somme_color = [somme_color[0]+col[0]/nb_pixel,somme_color[1]+col[1]/nb_pixel,somme_color[2]+col[2]/nb_pixel]
+                colors.append(somme_color[0]+somme_color[1]+somme_color[2])
+                Liste_barycentres.append([int(floor((haut_max+bas_max)/2)),int(floor((gauche_max+droite_max)/2)), int(floor((haut_max - bas_max)/2)), int(floor((droite_max - gauche_max)/2)), nb_pixel])   
 
+ 
+    
+def echelle(colors,Liste_barycentres):
+    m,i = max_liste(colors)
+    hauteur_pix = Liste_barycentres[i][3]*2
+    print("hauteur pix = ", hauteur_pix)
+    #print("min =", i)
+    taille_pixel = 0.24/hauteur_pix
+    return(taille_pixel, i)
+
+echelle = echelle(colors, Liste_barycentres)
+print(echelle)
+#print(colors)
+#print(Liste_barycentres)
 
 def proche_gris(pixel1):
     return sqrt((pixel[0]-pixel[1])**2+(pixel[0]-pixel[2])**2+(pixel[2]-pixel[1])**2)<50
 
     
-    
+
     
     
 
@@ -142,9 +166,9 @@ for m in range(len(Liste_barycentres)-1):
             if(0<Liste_barycentres[m][0]+i-Liste_barycentres[m][2]+1<l) and (0<Liste_barycentres[m][1]+j-Liste_barycentres[m][3]+1<c):
                 imgchiffre.putpixel((Liste_barycentres[m][0]+i-Liste_barycentres[m][2]+1,Liste_barycentres[m][1]+j-Liste_barycentres[m][3]+1), chiffre.getpixel((i,j)))
 
-imgNtemp.show()
+#imgNtemp.show()
 imgNtemp.save('imgNtemp.png')
-imgchiffre.show()
+#imgchiffre.show()
 imgchiffre.save('imgchiffre.png')
 
 #à partir de la liste des prises données par l'utilisateur, renvoie la voie sélectionnée à titre de confirmation
@@ -167,7 +191,4 @@ def print_voie(L):
                     imgNN.putpixel((Liste[0]+i-Liste[2]+1,Liste[1]+j-Liste[3]+1), pixel)
     return Liste
    
-imgNN.save('voie_L.png')
-imgNN.show()
-
                                 
