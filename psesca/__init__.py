14203720c1fad936e292dd1a0d1dcd1d8e471c7c
Étienne Morice <pro.emorice@mail.com>
2018-03-17 18:21:45 +0100

    Adding the possibility of externalizing native modules in a different psesca_ext module

--- psesca/__init__.py
+++ psesca/__init__.py
@@ -1,4 +1,17 @@
+try:
+    # Warning : Here, import order matters !
+    import psesca_ext.shape as shape
+    import psesca_ext.wha_shape as wha_shape
+    import psesca_ext.route as route
+    import psesca_ext.stancegraph as stancegraph
+    import psesca_ext.morphology as morphology
+    import psesca_ext.climbermodel as climbermodel
+    import psesca_ext.controller as controller
+except ImportError:
+    pass
+
 from .application import Application
 from .factory import Factory
 from .defaultfactory import DefaultFactory
 from .dummycontroller import DummyController
+
